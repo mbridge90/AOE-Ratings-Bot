71cc9fb4f8a9853765d172a39d660b3b90aac579
Margaret Bridge <60153746+mbridge90@users.noreply.github.com>
2020-06-05 17:49:11 +0100

    Added erro handling and additional stat request

--- bot.py
+++ bot.py
@@ -23,19 +23,52 @@ async def on_message(message):
     if message.author == bot.user:
         return
 
-    if 'rating!' in message.content:
+    if '!rating' in message.content:
         as_list = message.content.split()
         userID = as_list[1]
-        response = requests.get(
-            'https://aoe2.net/api/player/ratinghistory',
-            params={'game': 'aoe2de',
-                    'leaderboard_id': '4',
-                    'start': '0',
-                    'count': '1',
-                    'steam_id': userID,
-                    }
-        )
-        data = response.json()
-        await message.channel.send(data[0]['rating'])
+
+        try:
+            TGresponse = requests.get(
+                'https://aoe2.net/api/player/ratinghistory',
+                params={'game': 'aoe2de',
+                        'leaderboard_id': '4',
+                        'start': '0',
+                        'count': '1',
+                        'steam_id': userID,
+                        }
+                )
+            if TGresponse.status_code == 200:
+                TGdata = TGresponse.json()
+                TGinfo = TGdata[0]['rating']
+            else:
+                await message.channel.send("Sorry, I can't find any information for that ID. 😳 \nMaybe there's a mistake in it? ")
+                return
+
+        except(IndexError):
+            TGinfo = 'No data available'
+
+
+        try:
+            onevoneresponse = requests.get(
+                    'https://aoe2.net/api/player/ratinghistory',
+                    params={'game': 'aoe2de',
+                            'leaderboard_id': '3',
+                            'start': '0',
+                            'count': '1',
+                            'steam_id': userID,
+                            }
+                )
+            onevonedata = onevoneresponse.json()
+            oneVoneinfo = onevonedata[0]['rating']
+        except(IndexError):
+            oneVoneinfo = 'No data available'
+
+        await message.channel.send(f'Player: {message.author}\n1v1: {oneVoneinfo}\nTG: {TGinfo}')
+
+        '''except(JSONDecodeError):
+            await message.channel.send("Sorry, I can't find any information for that ID. 😳 \nMaybe there's a mistake in it? ")'''
+
+    if 'good bot' in message.content.lower():
+        await message.channel.send('Thank you! 😇')
 
 bot.run(TOKEN)
