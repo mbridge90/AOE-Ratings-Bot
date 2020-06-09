a7669e6f174ea31c5690bf23b1e156abe43ba89a
Margaret Bridge <60153746+mbridge90@users.noreply.github.com>
2020-06-09 01:03:25 +0100

    added files for Heroku

---
+++ playerdict.py
@@ -0,0 +1,11 @@
+"""playerdict = {"RibRidge": "76561198047991259",
+"JimmyPiddle": "76561198121147364",
+"DoctorEmmet": "76561198168974470",
+"CrazyFlash": "76561198025363811",
+"JohnLemon": "76561198032979191"}"""
+
+playerdict = {"76561198047991259": "RibRidge",
+"76561198121147364": "JimmyPiddle",
+"76561198168974470": "DoctorEmmet",
+"76561198025363811": "CrazyFlash",
+"76561198032979191": "JohnLemon"}
