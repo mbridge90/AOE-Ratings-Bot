ab6f916bf3c07d7e3a9d1ae795930db8d5a51894
Margaret Bridge <60153746+mbridge90@users.noreply.github.com>
2020-06-23 17:12:34 +0100

    Add troll John Lennon

--- bot.py
+++ bot.py
@@ -8,16 +8,16 @@ from dotenv import load_dotenv
 
 from discord.ext import commands
 
-
-
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
 bot = commands.Bot(command_prefix='!')
 
+
 @bot.event
 async def on_ready():
     print(f'{bot.user} has connected to Discord!')
+
 
 @bot.event
 async def on_message(message):
@@ -32,7 +32,8 @@ async def on_message(message):
             await message.channel.send(f"Please enter an ID number. I can't do anything without one yet!")
             return
         if userID not in playerdict.keys():
-            await message.channel.send(f"Sorry, I can't find any information for that ID ({userID}). 😳 \nMaybe there's a mistake in it? ")
+            await message.channel.send(
+                f"Sorry, I can't find any information for that ID ({userID}). 😳 \nMaybe there's a mistake in it? ")
 
         else:
             try:
@@ -44,7 +45,7 @@ async def on_message(message):
                             'count': '1',
                             'steam_id': userID,
                             }
-                    )
+                )
                 if TGresponse.status_code == 200:
                     TGdata = TGresponse.json()
                     TGinfo = TGdata[0]['rating']
@@ -52,17 +53,16 @@ async def on_message(message):
             except(IndexError):
                 TGinfo = 'No data available'
 
-
             try:
                 onevoneresponse = requests.get(
-                        'https://aoe2.net/api/player/ratinghistory',
-                        params={'game': 'aoe2de',
-                                'leaderboard_id': '3',
-                                'start': '0',
-                                'count': '1',
-                                'steam_id': userID,
-                                }
-                    )
+                    'https://aoe2.net/api/player/ratinghistory',
+                    params={'game': 'aoe2de',
+                            'leaderboard_id': '3',
+                            'start': '0',
+                            'count': '1',
+                            'steam_id': userID,
+                            }
+                )
                 onevonedata = onevoneresponse.json()
                 oneVoneinfo = onevonedata[0]['rating']
             except(IndexError):
@@ -70,11 +70,14 @@ async def on_message(message):
 
             await message.channel.send(f'Player: {playerdict[userID]}\n1v1: {oneVoneinfo}\nTG: {TGinfo}')
 
-
     if 'good bot' in message.content.lower():
         await message.channel.send('Thank you! 😇')
 
     if 'bad bot' in message.content.lower():
-        await message.channel.send('😭😭😭😭😭😭')
+        if message.author == "JLemon":
+            await message.channel.send('🍋🍋🍋🍋🍋🍋')
+        else:
+            await message.channel.send('😭😭😭😭😭😭')
+
 
 bot.run(TOKEN)
